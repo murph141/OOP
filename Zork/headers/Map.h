5c37a97d1446f5a09017fe8e469ec2bf274f9a8c
Eric Murphy <murph141@purdue.edu>
2015-12-05 16:06:35 -0500

    Temp saying point

---
+++ Zork/headers/Map.h
@@ -0,0 +1,13 @@
+#ifndef __MAP_H_
+#define __MAP_H_
+
+class Map
+{
+  private:
+    // Add stuff
+
+  public:
+    // Add stuff
+};
+
+#endif // __MAP_H_
---
+++ Zork/source/main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+#include "rapidxml/rapidxml.hpp"
+#include "rapidxml/rapidxml_utils.hpp"
+#include "rapidxml/rapidxml_print.hpp"
+
+using namespace std;
+using namespace rapidxml;
+
+int main(int argc, char * argv[])
+{
+  file<> xmlFile("inputs/sample.xml");
+  xml_document<> doc;
+  xml_node<> * node;
+
+  doc.parse<0>(xmlFile.data());
+
+  node = doc.first_node();
+
+  for(xml_node<> * tag = node->first_node(); tag; tag = tag->next_sibling())
+  {
+    cout << *tag << endl;
+  }
+
+  return(0);
+}
