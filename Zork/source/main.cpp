fba5f8e2d832425b8412f2f687a64e4b1077ec3a
Eric Murphy <murph141@purdue.edu>
2015-12-05 16:44:12 -0500

    Moving locations

--- Zork/source/main.cpp
+++ Zork/source/main.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<string>
+#include<sstream>
 
 #include "../rapidxml/rapidxml.hpp"
 #include "../rapidxml/rapidxml_utils.hpp"
@@ -7,9 +9,22 @@
 using namespace std;
 using namespace rapidxml;
 
+void fileParse(string);
+
 int main(int argc, char * argv[])
 {
-  file<> xmlFile("inputs/sample.xml");
+  fileParse("inputs/sample.xml");
+
+  return(0);
+}
+
+
+// Begin file parsing
+void fileParse(string inputFile)
+{
+  // Check if file exists and you have read permission (TODO)
+  
+  file<> xmlFile(inputFile.c_str());
   xml_document<> doc;
   xml_node<> * node;
 
@@ -19,8 +34,30 @@ int main(int argc, char * argv[])
 
   for(xml_node<> * tag = node->first_node(); tag; tag = tag->next_sibling())
   {
-    cout << *tag << endl;
+    ostringstream ss;
+    ss << (*tag).name();
+    string s = ss.str();
+    
+    // Detrmine which type it is
+    if(!s.compare("room"))
+    {
+      //roomParse()
+    }
+    else if(!s.compare("item"))
+    {
+      //itemParse()
+    }
+    else if(!s.compare("container"))
+    {
+      //containerParse
+    }
+    else if(!s.compare("creature"))
+    {
+      //creatureParse
+    }
+    else
+    {
+      //Error
+    }
   }
-
-  return(0);
 }
